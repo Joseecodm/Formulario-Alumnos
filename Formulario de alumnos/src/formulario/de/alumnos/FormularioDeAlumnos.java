e650e1e6a1a7b607dfcde20f3b8a7a2d26e65909
Jose Manuel Cortes Ceron <230110688@itsoeh.edu.mx>
2024-01-25 10:54:45 -0600

    Subi el proyecto

---
+++ Formulario de alumnos/src/formulario/de/alumnos/FormularioDeAlumnos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formulario.de.alumnos;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Josee
+ */
+public class FormularioDeAlumnos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+         // Crea una instancia de tu JFrame
+        Formulario Form = new Formulario();
+        
+        // Hacer visible el JFrame
+        Form.setVisible(true);
+
+    }
+    
+}
